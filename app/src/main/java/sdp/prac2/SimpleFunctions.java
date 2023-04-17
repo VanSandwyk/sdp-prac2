562c97a088b748a8ad85d3cb80add4a20cd937f8
Jaydin <jfischat13@gmail.com>
2023-04-17 20:45:51 +0200

    Added Task2

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -16,6 +16,31 @@ public class SimpleFunctions {
     return total; // Return the total sum
     }
 
+    // Author: JuneGemini13
+    // StudentId: G21F7853
+    // Additon: Method Task2, which removes the first character from
+    // every element in a list. If an element has no characters, remove it from
+    // the output.
+
+    public static String[] task2(String[] inputArray) {
+        // create a new array for the output, with the same length as the input array
+        String[] outputArray = new String[inputArray.length];
+        // initialize an output index counter
+        int outputIndex = 0;
+        
+        // iterate over each element in the input array
+        for (String s : inputArray) {
+            // check if the element has more than one character
+            if (s.length() > 1) {
+                // add a new string to the output array with the first character removed
+                outputArray[outputIndex++] = s.substring(1);
+            }
+        }
+        
+        // create a new array with only the elements that were added to the output array
+        return Arrays.copyOf(outputArray, outputIndex);
+    }
+
     public static List<Integer> Task4(List<Integer> a, List<Integer> b){
     //If lists are different sizes, return null
     if (a.size() != b.size()){
